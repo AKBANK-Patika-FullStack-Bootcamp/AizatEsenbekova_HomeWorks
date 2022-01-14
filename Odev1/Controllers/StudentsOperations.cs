cc2401d895d141ec3169ae336e9baaf9d8c7d8de
Aizat111 <aizaesen4@gmail.com>
2022-01-14 14:00:37 +0300

    Logging

--- Odev1/Controllers/StudentsOperations.cs
+++ Odev1/Controllers/StudentsOperations.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Odev1.Model;
-
 
 namespace Odev1.Controllers
 {
@@ -8,64 +7,44 @@ namespace Odev1.Controllers
     [Route("[controller]")]
     public class StudentsOperationsController : ControllerBase
     {
-        //Defining global variables
-        List<Student> resultList = new List<Student>();
+        List<Student> resultList = new List<Student>();//bulisteyi bir kac yerde kullandigimiz icin global yaptim
         Result result=new Result();
-        LoggerCls logCls = new LoggerCls();
-      
-
         [HttpGet]
-        //Show list of all students
+        //Butun ogrencilerin listedini goster
         public List<Student> getStudents()
         {
-            //Filling the list of students
+            
             resultList = AddUser();
-            //Sending back the list of students
-
-            //Save process to Logging file
-            logCls.CreateLog("Bütün öğrencilerin listesi görüntülendi");
             return resultList;
-
-        
         }
 
         [HttpGet("{id}")]
-        //Show only the student whose id is specified
+        //Sadece idsi belirtilen ogrenciyi goster
         public Student getStudent(int id)
         {
             
             resultList = AddUser();
             Student student = new Student();
-            //Find specified student with id
             student = resultList.FirstOrDefault(x => x.StudentId == id);
-            //Save process to Logging file
-            logCls.CreateLog(id + " nolu öğrencinin bilgileri görüntülendi");
-            return student; 
-
-      
+            return student;
         }
-        
+
         [HttpPost]
-        //Add student to list
+        //Listeye ogrenci ekle
         public Result PostStudents(Student ogr)
         {
-            //Filling the list of students
+            //Liste dolduruluyor
             resultList = AddUser();
-
-            //Is the student we want to add on the list?
+        
+            //Eklemek istedigimiz ogrenci listede var mi?
             bool control = resultList.Select(x => x.StudentId == ogr.StudentId).FirstOrDefault();
-            //if answer is "NO" add student to the list
             if(control==false)
             {
-               
+                //Listeye yeni ogrenci ekleniyor
                 resultList.Add(ogr);
                 result.status = 1;
                 result.message = "Listeye ogrenci basariyla eklendi";
-
-                //Save process to Logging file
-                logCls.CreateLog(ogr.StudentId + " nolu ogrenci listeye başarıyla eklendi");
             }
-            // return failed message and all list of students
             else
             {
                 result.status = 0;
@@ -79,28 +58,23 @@ namespace Odev1.Controllers
         }
 
         [HttpPut("{id}")]
-        //Update the information of the student whose id is specified in the list
+        //Litedeki ogrencinin bilgilerini guncelle
         public Result UpdateStudent(Student newValue, int id)
         {
-            //Filling the list of students
-            resultList = AddUser();
-
-            //Find student with specified id
-            //student may not be on the list
-            Student? oldValue=resultList.Find(y=>y.StudentId ==id);
+            resultList=AddUser();
+            //Kullanici tarafindan idsi alinan ogrencini listeden buluyoruz
+            Student ? oldValue=resultList.Find(y=>y.StudentId ==id);
             if(oldValue!=null)
             {
-                //if student on the list update student's unformation 
+                //Eger ogrenci listede varsa bilgilerini guncelliyoruz
+                resultList.Remove(oldValue); 
+                resultList.Add(newValue);
                 result.status = 1;
-                result.message = "öğrenciler listesi basariyla guncellendi";
+                result.message = "Ogrenciler listesi basariyla guncellendi";
                 result.students = resultList;
-
-                //Save process to Logging file
-                logCls.CreateLog(id + " nolu ogrencinin bilgileri ve " + result.message);
             }
             else
             {
-                //else return failed message
                 result.status = 0;
                 result.message = "Bilgisini guncellemek istediginiz ogrenci listede bulunamadi";
             }
@@ -109,39 +83,30 @@ namespace Odev1.Controllers
         }
 
         [HttpDelete("{id}")]
-        //Delete the information of the student whose id is specified in the list
+        //Listeden ogrenci sil
         public Result DeleteStudent(int id)
         {
-            //Filling the list of students
+            //her seferinde listeyi doldur
             resultList = AddUser();
-
-            //Find student with specified id
-            //student may not be on the list
-            Student? deletedValue=resultList.Find(y=>y.StudentId==id);
+            Student ? deletedValue=resultList.Find(y=>y.StudentId==id);
             if (deletedValue != null)
             {
-                //if student on the list delete student's information from list
+                //Eger ogrenci listede varsa siliyoruz
                 resultList.Remove(deletedValue);
                 result.status = 1;
-                result.message = "Ogrenci başarıyla silindi";
+                result.message = "Ogrenciler listesi basariyla guncellendi";
                 result.students = resultList;
-                logCls.CreateLog(id + " nolu " + result.message);
-                
-               
-
             }
             else
             {
-                //else return failed message
                 result.status = 0;
                 result.message = "Bilgisini guncellemek istediginiz ogrenci listede bulunamadi";
             }
             return result;
-            
+
         }
        
-        //fuction which filling students' information
-        //use this function if you don't have database 
+        //ogrenciler listesini burada dolduruyoruz 
         public List<Student> AddUser()
         {
             List<Student> lst = new List<Student>();
