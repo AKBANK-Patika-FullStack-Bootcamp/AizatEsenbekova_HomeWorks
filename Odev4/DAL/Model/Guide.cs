2bbd7177b4f6c6d1526ef49d326208c65b8d2d2b
Aizat111 <aizaesen4@gmail.com>
2022-01-15 12:25:56 +0300

    Revert "new project for db connections"
    This reverts commit b5c00d611dcdf1014c4bb88ffdb6cbcb37509ea8.

---
+++ Odev4/DAL/Model/Guide.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Model
+{
+    public class Guide
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int AddressId { get; set; }
+        public string TelNo { get; set; }
+    }
+}
---
+++ Odev4/DAL/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int GuideId { get; set; }
+        public int AddressId { get; set; }
+    }
+}
