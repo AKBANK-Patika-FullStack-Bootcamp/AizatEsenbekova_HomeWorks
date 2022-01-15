afc4ef3d1227fddccbb0fc4af707a82f465cbff0
Aizat111 <aizaesen4@gmail.com>
2022-01-15 12:16:58 +0300

    new project for db connections

---
+++ Odev4/DAL/Model/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL.Model
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ Odev4/db_connection/Controllers/StudentsOperations.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Model;
+using Microsoft.AspNetCore.Mvc;
+namespace db_connection.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsOperations : ControllerBase
+    {
+        List<Student> resultList= new List<Student>();
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            resultList = AddStudents();
+            return resultList;
+        }
+        [HttpPost]
+        public List<Student> AddStudents()
+        {
+            List<Student>lst = new List<Student>();
+            lst.Add(new Student { Id = 1, Name= "Aizat", LastName= "Esen", Age= 21,AddressId= 1,GuideId= 1 });
+            lst.Add(new Student { Id = 2, Name = "Ela", LastName = "Presen", Age = 23, AddressId = 2, GuideId = 3 });
+            return lst;
+        }
+    }
+}
