9c738b62c38fec518ae3fc98ad4e53405973a360
AizatEsenbekova <aizaesen4@gmail.com>
2022-02-11 09:53:52 +0300

    axios

--- ReactLessonsRepeat/week2/pokemon/src/App.js
+++ ReactLessonsRepeat/week2/pokemon/src/App.js
@@ -1,15 +1,28 @@
 import Pokemon from "./components/Pokemon";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
-  const pokemonList=[
+  /*const pokemonList=[
     {name:"Aizat", id:1, height:157},
     {name:"MK", id:2, height:180},
     {name:"Anisa", id:3, height:162},
     {name:"Zalkar", id:4, height:undefined} //undefined olunca default değeri almış oluyor
-  ]
+  ]*/
+  const [pokeList, setPokeList]=useState([]);
   const [isActive , setActive]= useState();
+
+  useEffect(()=>{axios.get("https://pokeapi.co/api/v2/pokemon")
+  .then((response)=>{
+  console.log(response.data.results);
+  const tempList=response.data.results.slice(0,5);
+  const PokewithHeight=tempList.map((poke)=>{
+    return {...poke, height:Math.floor(Math.random()*10+1)}})
+  setPokeList(PokewithHeight);});
+},[])
+  
+ 
   return (
     <div className="App">
     <div className="App-header">
@@ -17,7 +30,7 @@ function App() {
       <Pokemon pokemonName={pokemonList[1].name} pokemonHeight={pokemonList[1].height}/>
       <Pokemon pokemonName={pokemonList[2].name} pokemonHeight={pokemonList[2].height}/>
       <Pokemon pokemonName={pokemonList[3].name} pokemonHeight={pokemonList[3].height}/> */
-      pokemonList.map((pokemon,index)=>
+      pokeList.map((pokemon,index)=>
       {
         return <Pokemon  isActive={isActive===pokemon.name} 
         setActive={setActive}
